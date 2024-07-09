9c0b3bbbd35aa175343ae58658bf102d8ab6c78b
geony_kkk <blueds05@gmail.com>
2024-07-10 08:57:57 +0900

    [feature] 23 page

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -9,3 +9,7 @@ class TestPrimeFactor(TestCase):
     def test_prime_factor_of_1(self):
         prime_factors = PrimeFactor()
         self.assertEqual([], prime_factors.of(1))
+
+    def test_prime_factor_of_2(self):
+        prime_factors = PrimeFactor()
+        self.assertEqual([2], prime_factors.of(2))
